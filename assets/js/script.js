6b3c2605392d83aae0227bc297116c3a2877d8c2
Brian Lalli <106120015+BrianLalli@users.noreply.github.com>
2022-06-30 20:38:57 -0500

    updated HTML, CSS, and JS with jQuery

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+// Pseudo-code
+// Landing page with start button
+// When start button is clicked, remove welcome msg and append question 1
+    // Add event listener
+        // Activity 12
+// Start timer
+// User answer question
+// Read chosen element, assess data value, compare to answer key
+// For loop for the questions
+    // If correct next question
+    // If incorrect subtract 10 sec from timer
+        // Activity
+// End of quiz
+// Add up user score
+// Display input for my user to type their initials
+// Second html and display user name and score
+// Read object from local storage
+    // Activity 24
+
+
+// Assignment Code
+// var generateBtn = document.querySelector("#generate");
+// List of questions, answer options, correct answers, and user answers
+var questionEl = []
+var answerOptionsEl = []
+var answersCorrectEl = []
+var userAnswersEl = []
+var timerEl = ()
+var startButtonEl = $('button-start');
+
+
+// var test = document.getElementById("button-start");
+
+
+startButtonEl.on('click', function () {
+    
