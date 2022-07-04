cdf11dbbbb89b7fcba5cd7067613421a6dc09147
Brian Lalli <106120015+BrianLalli@users.noreply.github.com>
2022-07-04 10:28:00 -0500

    start button working with timer and all questions coming up on screen

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # Code-Quiz
+
+
+## Project Description:
+The task was to create a random password generator for my coding bootcamp. First, the user must click the 'Generate Password' button to prompt a question which asks how many characters the user wants their password to be. Then it continues to ask conditional questions based on the type of characters the user wants to include: lowercase, uppercase, numeric, and special characters (standard is to include all but in this task we let the user decide). After, the program will randomly choose characters based on the user criteria and generate a password.
+
+
+
+#### You can access this project via [GitHub](https://github.com/BrianLalli/Random-Password-Generator)
+
+<br>
+
+
+
+## Mock Up:
+![Mock Up](../Module-3-Challenge-Random-Password-Generator/assets/images/IMG_0623.jpg)
+
+<br>
+
+## Pseudo-Code:
+* List of numbers, special characters, uppercase and lowercase
+* Write function for password input
+* Add event listener to generate button
+* Prompt = length
+* Create var to hold answer to prompt
+* Conditional if else statement to ensure user answer to prompt is within range
+* Confirm = lowercase, uppercase, numeric, special characters
+* Conditional if else to validate user answers and combine all arrays in 1 array
+* Function to randomly capture user determined # of characters from the combined array
+* Change function to a string to generate and return password
+
+<br>
+
+
+## Credits:
+[Ian Darland](https://github.com/iandarland)
+Thanks Ian!!
+
+<br>
+
+## Sources:
+[W3](https://www.w3schools.com/jsref/met_win_setinterval.asp)
+
+// src: 
+// https://releases.jquery.com/
+// 
+
+[jQuery](https://api.jquery.com/removeclass/)
+
+[Mozilla](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array)
+
+CSS Styling by: [Enroll Mind](https://enrollmind.com/random-password-generator-using-html-css-and-javascript/*/)
+
+<br>
+
+## MIT License:
+
+Copyright (c) 2022 Brian Lalli
--- assets/js/script.js
+++ assets/js/script.js
@@ -16,21 +16,126 @@
 // Second html and display user name and score
 // Read object from local storage
     // Activity 24
-
+debugger
 
 // Assignment Code
 // var generateBtn = document.querySelector("#generate");
 // List of questions, answer options, correct answers, and user answers
 var questionEl = []
 var answerOptionsEl = []
-var answersCorrectEl = []
-var userAnswersEl = []
-var timerEl = ()
-var startButtonEl = $('button-start');
+// var answersCorrectEl = [];
+var userAnswersEl = [];
+var timerEl = $("#timerEl");
+var startButtonEl = $('#button-start');
+var startingNumber = 60;
+var correct = ["Mercury", "Hotmail", "Brazil", "Bing", "Gemini"];
+
+var questionNumber = 0;
+
+var questionsList = [
+    {
+        text: "Hg is the chemical symbol of which element?",
+        choices: ["Iron", "Magnesium", "Calcium", "Mercury"],
+        correct: "Mercury"
+    },
+    {
+        text: "Which email service is owned by Microsoft?",
+        choices: ["Yahoo", "Superhuman", "Hotmail", "Gmail"],
+        correct: "Hotmail"
+    },
+    {
+        text: "Which country produces the most coffee in the world?",
+        choices: ["Peru", "Colombia", "Brazil", "China"],
+        correct: "Brazil"
+    },
+    {
+        text: "What is Chandler’s last name in the sitcom Friends?",
+        choices: ["Bong", "Bing", "Bang", "Bung"],
+        correct: "Bing"
+    },
+    {
+        text: "What is the third sign of the zodiac?",
+        choices: ["Gemini", "Libra", "Pisces", "Capricorn"],
+        correct: "Gemini"
+    },
+]
 
 
-// var test = document.getElementById("button-start");
+startButtonEl.on('click', letsPlay)
+function letsPlay(){
+    console.log("Button test");
+    $("#Q").removeClass("d-none");
+    timerStart();
+    nextQuestion()
+}
 
+function timerStart(){
+    setInterval(function () {
+    if(startingNumber > 0){
+    startingNumber = startingNumber -1
+    timerEl.text(startingNumber);
+    }
+    }, 1000);
+}
 
-startButtonEl.on('click', function () {
-    
+function nextQuestion() {
+    $("#questionText").text(questionsList[questionNumber].text)
+    $("#choice1").text(questionsList[questionNumber].choices[0])
+    $("#choice2").text(questionsList[questionNumber].choices[1])
+    $("#choice3").text(questionsList[questionNumber].choices[2])
+    $("#choice4").text(questionsList[questionNumber].choices[3])
+}
+{
+    $("#choice1").on("click", function() {
+        questionNumber++;
+        nextQuestion();
+        console.log("choice1");
+        if ("#choice1" === correct) {
+            ;
+            console.log('Correct');
+        } else {
+            ;
+            console.log('Wrong');
+            startingNumber = startingNumber -10;
+        }
+    })
+    $("#choice2").on("click", function() {
+        questionNumber++;
+        nextQuestion();
+        console.log("choice2");
+        if ("#choice2" === correct) {
+            ;
+            console.log('Correct');
+        } else {
+            ;
+            console.log('Wrong');
+            startingNumber = startingNumber - 10;
+        }
+    })
+    $("#choice3").on("click", function() {
+        questionNumber++;
+        nextQuestion();
+        console.log("choice3");
+        if ("#choice3" === correct) {
+            ;
+            console.log('Correct');
+        } else {
+            ;
+            console.log('Wrong');
+            startingNumber = startingNumber - 10;
+        }
+    })
+    $("#choice4").on("click", function() {
+        questionNumber++;
+        nextQuestion();
+        console.log("choice4");
+        if ("#choice4" === correct) {
+            ;
+            console.log('Correct');
+        } else {
+            ;
+            console.log('Wrong');
+            startingNumber = startingNumber - 10;
+        }
+    })
+}
